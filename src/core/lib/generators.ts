a32e3e8dbbbbfc96c4ee7c2ba1ce226daf319b5c
Alish Baig <alishbaig2004@gmail.com>
2025-09-28 21:17:31 +0530

    refactor: remove refactor whole file structure.

--- src/core/lib/generators.ts
+++
@@ -1,8 +0,0 @@
-export const generateRandomId = () => {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let randomId = '';
-    for (let i = 0; i < 16; i++) {
-        randomId += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return randomId;
-}
