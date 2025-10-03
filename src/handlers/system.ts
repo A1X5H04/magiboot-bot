b7b2e9c769df5c2754d713502b6f921d1ee90d85
Alish Baig <alishbaig2004@gmail.com>
2025-10-03 17:41:35 +0530

    feat: create handlers for private-chat

---
+++ src/handlers/system.ts
@@ -0,0 +1,11 @@
+import { FormattedString } from "@grammyjs/parse-mode";
+import { AppContext } from "../types/context";
+
+export async function handlePrivateChat(ctx: AppContext) {
+    const greetingMessage = FormattedString.b("👋 Hi, I am Magiboot bot.\nI'll help you create bootanimation module from a video.")
+    .plain("\n\nUnfortunately, I don't talk to strangers in private.\nTo create bootanimation modules for your own video, join the @magibootchat group, and @magiboot channel where I post bootanimations modules using videos send by users.\n\n")
+    .italic("For more information on how to use me click ")
+    .link("here", "https://telegra.ph/Magiboot-BOT-Guide-08-11")
+
+    ctx.reply(greetingMessage.text, { entities: greetingMessage.entities, link_preview_options: { is_disabled: true } })
+}
