092101951176a2a2be6878788b612ab7d82cae20
Alish Baig <alishbaig2004@gmail.com>
2025-10-03 17:38:28 +0530

    refactor: simplify the code

--- src/core/bot.ts
+++ src/core/bot.ts
@@ -1,25 +1,19 @@
 import { Bot} from 'grammy';
 import { Env } from '../types/cloudflare';
 
-import { CustomContext } from '../types/context';
+import { AppContext } from '../types/context';
 
 import commandComposer from './commands';
+import { privateMessageMiddleware } from '../middlewares/private-message';
+import { extendContextMiddleware } from '../middlewares/extend-ctx';
 
 async function initBot(env: Env) {
-    const bot = new Bot<CustomContext>(env.BOT_TOKEN, { botInfo: JSON.parse(env.BOT_INFO) });
+    const bot = new Bot<AppContext>(env.BOT_TOKEN, { botInfo: JSON.parse(env.BOT_INFO) });
 
     // Extend context with environment
-    bot.use((ctx, next) => {
-        ctx.env = env;
-        return next();
-    })
-
-    // bot.use(session({
-    //     initial: () => {
-    //         return { routerHistory: [], formState: {} }
-    //     },
-    //     storage: grammyD1StorageAdapter,
-    // }))
+    bot.use(extendContextMiddleware({ env }))
+    bot.on("message", privateMessageMiddleware);
+    
     bot.use(commandComposer)
 
     return bot;
